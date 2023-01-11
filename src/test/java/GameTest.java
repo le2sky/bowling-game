f10195f1270de4519528ff1eb092d0a61c56cb4d
leesky <leehaneul0623@gmail.com>
2023-01-11 19:15:46 +0900

    2.3 refactor

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,10 +1,17 @@
+import org.junit.Before;
 import org.junit.Test;
 
 public class GameTest {
 
+  private Game game;
+
+  @Before
+  public void setUp() throws Exception {
+    game = new Game();
+  }
+
   @Test
   public void canCreate() {
-    Game game = new Game();
   }
 
   @Test
