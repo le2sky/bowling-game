c46786ee615b82f23b87fac0c0187e4277530657
leesky <leehaneul0623@gmail.com>
2023-01-11 19:45:05 +0900

    5.3 refactoring

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,12 +1,20 @@
 public class Game {
 
-  private int score = 0;
+  private int[] rolls = new int[21];
+  private int currentRoll = 0;
 
   public void roll(int pins) {
-    this.score += pins;
+    rolls[currentRoll++] = pins;
   }
 
   public int getScore() {
-    return this.score;
+    int score = 0;
+    int i = 0;
+    for (int frame = 0; frame < 10; frame++) {
+      score += rolls[i] + rolls[i + 1];
+      i += 2;
+    }
+
+    return score;
   }
 }
