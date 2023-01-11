1ada5c2650026a7c9f23e10d4f18176982291c56
leesky <leehaneul0623@gmail.com>
2023-01-11 19:20:21 +0900

    3.1 add failing test

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,3 +1,8 @@
+import static org.hamcrest.core.Is.*;
+import static org.junit.Assert.assertThat;
+
+import org.hamcrest.core.Is;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -17,5 +22,13 @@ public class GameTest {
   @Test
   public void canRoll() {
     game.roll(0);
+  }
+
+  @Test
+  public void gutterGame() {
+    for (int i = 0; i < 20; i++)
+      game.roll(0);
+
+    assertThat(game.getScore(), is(0));
   }
 }
