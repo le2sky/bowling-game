3c58968d58adde17c37e6ea20c12001d1e0eacda
leesky <leehaneul0623@gmail.com>
2023-01-11 19:52:53 +0900

    7.3 refactoring - rename i to firstFrame

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -9,14 +9,14 @@ public class Game {
 
   public int getScore() {
     int score = 0;
-    int i = 0;
+    int firstRollInFrame = 0;
     for (int frame = 0; frame < 10; frame++) {
-      if (rolls[i] + rolls[i + 1] == 10) {
-        score += 10 + rolls[i + 2];
-        i += 2;
+      if (rolls[firstRollInFrame] + rolls[firstRollInFrame + 1] == 10) {
+        score += 10 + rolls[firstRollInFrame + 2];
+        firstRollInFrame += 2;
       } else {
-        score += rolls[i] + rolls[i + 1];
-        i += 2;
+        score += rolls[firstRollInFrame] + rolls[firstRollInFrame + 1];
+        firstRollInFrame += 2;
       }
     }
 
