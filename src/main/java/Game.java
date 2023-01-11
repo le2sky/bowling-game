39604a3acb4c473ec97d548fb347daa707775a73
leesky <leehaneul0623@gmail.com>
2023-01-11 19:24:42 +0900

    4.2 make it pass

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,9 +1,12 @@
 public class Game {
 
+  private int score = 0;
+
   public void roll(int pins) {
+    this.score += pins;
   }
 
   public int getScore() {
-    return 0;
+    return this.score;
   }
 }
